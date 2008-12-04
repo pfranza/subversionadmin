b2fc9c9bdb17106186d27e723f6eab44b8569784
pfranza <pfranza@c26fd638-5356-0410-9f2a-01aca1e7f826>
2008-12-04 01:58:32 +0000

    settings in properties file

--- trunk/server/src/com/peterfranza/svnadmin/server/ChangeSetEmailer.java
+++ trunk/server/src/com/peterfranza/svnadmin/server/ChangeSetEmailer.java
@@ -5,9 +5,19 @@ import com.peterfranza.svnadmin.server.ChangeMiner.ChangeSummary;
 public class ChangeSetEmailer {
 
 	public static void main(String[] args) {
-		
-		ChangeSummary set = ChangeMiner.getSummary("http://subversionadmin.googlecode.com/svn/trunk", "", "", 27);
+
+		ChangeSummary set = ChangeMiner.getSummary(ApplicationProperties
+				.getProperty("repository_url"), ApplicationProperties
+				.getProperty("repository_username"), ApplicationProperties
+				.getProperty("repository_password"), Long.valueOf(args[0]));
+
 		System.out.println(set);
+		String[] to = { "pfranza@gmail.com" };
+		String subject = "svn commit: " + set.getMessage();
+		MailSender.sendMail(ApplicationProperties.getProperty("smtp_server"),
+				"pfranza@home.com", to,
+				subject.length() < 100 ? subject
+						: (subject.substring(0, 100) + "..."), set.toString());
 	}
-	
+
 }
