b1e82359d77a6bb173da99b0b59b407ccec4c82f
pfranza <pfranza@c26fd638-5356-0410-9f2a-01aca1e7f826>
2008-12-31 19:11:49 +0000

    allow regular user to change password without logging in to the system

--- source/interface/com/gorthaur/svnadmin/client/rpcinterface/AuthenticationInterfaceAsync.java
+++ source/interface/com/gorthaur/svnadmin/client/rpcinterface/AuthenticationInterfaceAsync.java
@@ -6,4 +6,6 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public interface AuthenticationInterfaceAsync {
     void authenticate(String username, String password, AsyncCallback<Boolean> result);
     void isAdmin(String username, AsyncCallback<Boolean> result);
+    void authenticateBasic(String username, String password, AsyncCallback<Boolean> result);
+    void changePasswordBasic(String username, String oldPassword, String newPassword, AsyncCallback<String> result);
 }
--- source/server/com/peterfranza/svnadmin/server/rpc/Authenticator.java
+++ source/server/com/peterfranza/svnadmin/server/rpc/Authenticator.java
@@ -10,11 +10,25 @@ public class Authenticator extends AsyncRemoteServiceServlet  implements Authent
 	private static final long serialVersionUID = -8920303489482728570L;
 
 	public boolean authenticate(String username, String password) {
-		return ACLOperationsDelegate.getInstance().getUserOperations().authenticate(username, password) && isAdmin(username);
+		return authenticateBasic(username, password) && isAdmin(username);
 	}
 
 	public boolean isAdmin(String username) {
 		return ACLOperationsDelegate.getInstance().getUserOperations().isAdmin(username);
+	}
+
+	public boolean authenticateBasic(String username, String password) {
+		return ACLOperationsDelegate.getInstance().getUserOperations().authenticate(username, password);
+	}
+
+	public String changePasswordBasic(String username, String oldPassword,
+			String newPassword) {
+		if(authenticateBasic(username, oldPassword)) {
+			ACLOperationsDelegate.getInstance().getUserOperations().setPassword(username, newPassword);
+			return "Password Changed";
+		} else {
+			return "Unable to authenticate old password";
+		}
 	}
 	
 }
